29cb1b11e23c0499ceed4d3371bfc3a328014e2d
RronProkupla <44345015+RronProkupla@users.noreply.github.com>
2019-01-04 14:11:08 +0100

    Vazhdo Valon

---
+++ scripts2.js
@@ -0,0 +1,23 @@
+var partIndex = 1;
+
+
+function plusPart(n) {
+  showSlides(partIndex += n);
+}
+
+
+
+function showParts(n) {
+  var i;
+  var parts = document.getElementsByClassName("part1");
+  if (n > parts.length) {partIndex = 1}    
+  if (n < 1) {partIndex = parts.length}
+  for (i = 0; i < parts.length; i++) {
+      parts[i].style.display = "none";  
+  }
+
+  parts[partIndex-1].style.display = "block"; 
+  
+}
+
+showParts(partIndex);
