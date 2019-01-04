562f5239d317c3f003910d001cbfd1ae7560f2a2
RronProkupla <44345015+RronProkupla@users.noreply.github.com>
2019-01-04 15:49:22 +0100

    vazhdo Valon

--- scripts2.js
+++ scripts2.js
@@ -1,24 +1,23 @@
 var partIndex = 1;
 
 
-function plusParts(r) {
-  showSlides(slideIndex += r);
+function plusPart(n) {
+  showSlides(partIndex += n);
 }
 
 
 
-function showParts(r) {
-  var j;
+function showParts(n) {
+  var i;
   var parts = document.getElementsByClassName("part1");
-
-  if (r > parts.length) {partIndex = 1}    
-  if (r < 1) {partIndex = parts.length}
-  for (j = 0; j < parts.length; j++) {
-      parts[j].style.display = "none";  
+  if (n > parts.length) {partIndex = 1}    
+  if (n < 1) {partIndex = parts.length}
+  for (i = 0; i < parts.length; i++) {
+      parts[i].style.display = "none";  
   }
 
   parts[partIndex-1].style.display = "block"; 
-   
+  
 }
 
 showParts(partIndex);
